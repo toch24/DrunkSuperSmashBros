862067bddf07de4ec8be3f88c12d2b6a8ac101d2
YukiKentyBonita <53498424+YukiKentyBonita@users.noreply.github.com>
2022-02-22 13:20:50 -0500

    Initialized database

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    roomCode = models.CharField(max_length=10, default="", unique=True)
+    roomHost = models.CharField(max_length=70, default="none", unique=True)
+    numPlayers = models.IntegerField(null = False, default=1)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from pymongo import MongoClient
 
-# Create your views here.
+#mongodb+srv://CIS4930:<password>@drunksupersmashbros.820dx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+# Password = DrunkSuperSmashBros
+# db_name = DrunkSuperSmashBros
+# username = CIS4930
+
+def get_db(db_name, host, port, username, password):
+
+ client = MongoClient(host=host,
+                      port=int(port),
+                      username=username,
+                      password=password
+                     )
+ db_handle = client[db_name]
+ return db_handle, client
 
 def test(request):
     return HttpResponse("Test Backend")
