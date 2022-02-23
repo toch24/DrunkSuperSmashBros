e00534c1808f68b30b1ebbe95d921ee8311027c7
Tommy Chong <tc18cj@my.fsu.edu>
2022-02-23 18:59:25 -0500

    Added a card in the center and header title

---
+++ frontend/src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Home(){
+    return(
+        <>
+        <h1 className='header'>DRUNK SUPER SMASH BROS</h1>
+        <div className="card">
+            <p>
+                This is a test of the App
+            </p>
+
+        </div>
+        </>
+
+    )
+}
+
+
+export default Home
