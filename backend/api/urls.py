2ad12692e4048bbc36e47894c32c1bb978fde43f
Tommy Chong <tc18cj@my.fsu.edu>
2022-03-30 14:17:21 -0400

    Connecting to the unofficial smash bros ultimate api for char data

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import new_lobby
+from .views import get_char_data, new_lobby
 urlpatterns = [
     path('new_lobby/', new_lobby),
+    path('get_data', get_char_data)
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,8 @@ from api.utils import get_db_handle
 from django.conf import settings
 import certifi
 from django.views.decorators.csrf import csrf_exempt
-
+import requests
+import json
 
 connection_string = 'mongodb+srv://CIS4930:DrunkSuperSmashBros@drunksupersmashbros.820dx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 # Password = DrunkSuperSmashBros
@@ -19,6 +20,7 @@ my_client = MongoClient(connection_string)
 dbname = my_client['DrunkSuperSmashBros']
 col_name = dbname["test"]
 
+
 @csrf_exempt
 def new_lobby(request):
     if request.method == "POST":
@@ -27,6 +29,15 @@ def new_lobby(request):
         print(name)
         print(code)
 
+    return HttpResponse(200)
+
+#get character data from the unofficial smash bros ultimate api
+@csrf_exempt
+def get_char_data(request):
+    r = requests.get('https://smashbros-unofficial-api.vercel.app/api/v1/ultimate/characters')
+    data = r.json()
+    for values in data:
+        print(values['name'])
     return HttpResponse(200)
 
 # from django.shortcuts import render
