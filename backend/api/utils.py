fd8330eeb0b9f9ea1e97b2982871fb4d7123d7ce
AtunBetun <beto7299@gmail.com>
2022-04-03 17:33:00 -0400

    Models comments on how to add more models without breaking. Fixed the migrations issue and resynced Database

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -7,11 +7,11 @@ from pymongo import MongoClient
 # username = CIS4930
 
 
-def get_db_handle(db_name, host, port, username, password):
-    client = MongoClient(host=host,
-                         port=int(port),
-                         username=username,
-                         password=password
-                         )
-    db_handle = client['db_name']
-    return db_handle, client
+# def get_db_handle(db_name, host, port, username, password):
+#     client = MongoClient(host=host,
+#                          port=int(port),
+#                          username=username,
+#                          password=password
+#                          )
+#     db_handle = client['db_name']
+#     return db_handle, client
