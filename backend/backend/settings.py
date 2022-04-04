f75b514999d66d06b397cb0730f120be525edcfc
Tommy Chong <tc18cj@my.fsu.edu>
2022-04-03 20:43:01 -0400

    Fixing websocket

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'backend.apps.ApiConfig',
     'rest_framework',
     'corsheaders',
+    'channels'
 
     
 ]
--- frontend/src/Utilities/FetchFunction.js
+++ frontend/src/Utilities/FetchFunction.js
@@ -20,23 +20,3 @@ export async function get_char_data(){
     
     return response.data
 }
-
-export async function get_room_code(code){
-    
-    return await code
-}
-
-export function get_code(){
-    const socket = new WebSocket(`ws://127.0.0.1:8080/ws/socket/`)
-
-
-    socket.onmessage = function(e){
-        let data = JSON.parse(e.data)
-        get_room_code(data['message'])
-        console.log(data)
-        //need to set newCode value with data here
-        //also need to redirect the client to a room page
-    }
-
-    return 200
-}
