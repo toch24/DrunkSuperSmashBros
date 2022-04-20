0d112aac5e3f86401f847cfdcdef91cd58ee9ab0
Tommy Chong <tommy242008@hotmail.com>
2022-04-20 18:40:57 -0400

    Solving static files not found error

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -153,15 +153,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'frontend', 'build', 'static')
+STATIC_URL = '/static/'
+if not DEBUG:
+        STATIC_ROOT = ''
+    
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'build', 'static')
-]
